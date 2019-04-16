2f4dc3b9ad11dfad235920e9c78ece5a9562b23a
Subfer <subfer@ukr.net>
2019-04-16 18:50:28 +0300

    Add autoprefixer to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
-var gulp      = require('gulp'), // Подключаем Gulp
-    sass        = require('gulp-sass'), //Подключаем Sass пакет,
+var gulp = require('gulp'), // Подключаем Gulp
+    sass = require('gulp-sass'), //Подключаем Sass пакет,
     browserSync = require('browser-sync'); // Подключаем Browser Sync
+    autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
 
-gulp.task('sass', function(){ // Создаем таск Sass
+gulp.task('sass', function () { // Создаем таск Sass
     return gulp.src('app/scss/**/*.scss') // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
 
-gulp.task('browser-sync', function() { // Создаем таск browser-sync
+gulp.task('browser-sync', function () { // Создаем таск browser-sync
     browserSync({ // Выполняем browserSync
         server: { // Определяем параметры сервера
             baseDir: 'app' // Директория для сервера - app
@@ -18,17 +20,17 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
     });
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
     return gulp.src('app/js/**/*.js')
-    .pipe(browserSync.reload({ stream: true }))
+        .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('code', function() {
+gulp.task('code', function () {
     return gulp.src('app/*.html')
-    .pipe(browserSync.reload({ stream: true }))
+        .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch('app/scss/**/*.scss', gulp.parallel('sass')); // Наблюдение за sass файлами
     gulp.watch('app/*.html', gulp.parallel('code')); // Наблюдение за HTML файлами в корне проекта
     gulp.watch('app/js/**/*.js', gulp.parallel('scripts')); // Наблюдение за JS файлом
